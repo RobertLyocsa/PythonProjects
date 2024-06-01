c2e1ce8ba3c35810ec69dca807feffa5d52d5731
Robert Lyocsa <robert.kristian.lyocsa@gmail.com>
2024-06-01 09:24:45 +0200

    // Commit changes adding if statement

--- PythonCodes/Python_Project.py
+++ PythonCodes/Python_Project.py
@@ -3,3 +3,7 @@ set_1 = {"apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"}
 print("pear" in set_1)
 print("banana" in set_1)
 print("lemon" in set_1)
+
+if 7 == len(set_1):
+    stringSet = str(set_1)
+    print("Right, You guessed my number! I have in my mind number " + str(len(stringSet)))
